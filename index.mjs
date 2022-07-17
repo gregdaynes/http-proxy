311f7356cea878e85495651569f281662110acff
Gregory Daynes <gregdaynes@gmail.com>
2022-07-16 23:03:39 -0700

    feat: minimal setup with flags, envars, and response

--- index.mjs
+++ index.mjs
@@ -1,20 +1,53 @@
+import { parseArgs } from "node:util"
+import { readFileSync } from 'node:fs'
 import Fastify from 'fastify'
+import envSchema  from 'env-schema'
+import S from 'fluent-json-schema'
 
-console.log(process.env.NODE_ENV)
-
-const fastify = Fastify({
-  logger: {
-    transport:
-      process.env.NODE_ENV === 'development'
-        ? {
-          target: 'pino-pretty',
-          options: {
-            translateTime: 'HH:MM:ss Z',
-            ignore: 'pid,hostname'
-          }
-        }
-        : undefined
-  }
+const envars = envSchema({
+  schema: S.object()
+    .prop('port', S.number().default(3000).required())
+    .prop('host', S.string().default('127.0.0.1').required())
+    .prop('cert', S.string())
+    .prop('key', S.string())
 })
 
-fastify.listen({ port: 3000 })
+const { values, positionals } = parseArgs({ options: {
+  port: {
+    type: "string",
+    short: "p"
+  },
+  host: {
+    type: "string",
+    short: "h"
+  },
+  cert: {
+    type: 'string',
+    short: 'c'
+  },
+  key: {
+    type: 'string',
+    short: 'k'
+  },
+}})
+
+const config = { ...envars, ...values, positionals }
+config.https = !!(config.key && config.cert)
+
+const fastify = Fastify({
+  https:
+    config.https
+      ? {
+        allowHTTP1: true,
+        key: readFileSync(config.key),
+        cert: readFileSync(config.cert)
+      }
+      : undefined,
+  logger: true
+})
+
+fastify.get('/', (request, reply) => {
+  return { Hello: "World" }
+})
+
+fastify.listen({ host: config.host, port: config.port })
